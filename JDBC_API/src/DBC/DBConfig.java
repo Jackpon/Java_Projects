9d15594f561c498ff015bce0443223c567e5acd2
Jackpon <JackpongWong@163.com>
2017-10-26 16:16:25 +0800

    Update DBConfig.java

--- JDBC_API/src/DBC/DBConfig.java
+++ JDBC_API/src/DBC/DBConfig.java
@@ -4,5 +4,5 @@ public interface DBConfig {
 	String driver="com.mysql.jdbc.Driver";
 	String url="jdbc:mysql://localhost:9806/projecttest";
 	String user="root";
-	String passwd="Jack0624";
+	String passwd="";
 }
